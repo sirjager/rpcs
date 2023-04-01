8f035592af2ba2e4a1d18a40892a4f1afebd52ba
Ankur Kumar <kankurcool@gmail.com>
2023-04-01 16:37:15 +0530

    updated

--- trueauth/js/rpc-login_pb.d.ts
+++ trueauth/js/rpc-login_pb.d.ts
@@ -1,5 +1,6 @@
 import * as jspb from 'google-protobuf'
 
+import * as google_protobuf_timestamp_pb from 'google-protobuf/google/protobuf/timestamp_pb';
 import * as user_pb from './user_pb';
 
 
@@ -31,6 +32,25 @@ export class LoginResponse extends jspb.Message {
   hasUser(): boolean;
   clearUser(): LoginResponse;
 
+  getSessionId(): string;
+  setSessionId(value: string): LoginResponse;
+
+  getAccessToken(): string;
+  setAccessToken(value: string): LoginResponse;
+
+  getRefreshToken(): string;
+  setRefreshToken(value: string): LoginResponse;
+
+  getAccessTokenExpiresAt(): google_protobuf_timestamp_pb.Timestamp | undefined;
+  setAccessTokenExpiresAt(value?: google_protobuf_timestamp_pb.Timestamp): LoginResponse;
+  hasAccessTokenExpiresAt(): boolean;
+  clearAccessTokenExpiresAt(): LoginResponse;
+
+  getRefreshTokenExpiresAt(): google_protobuf_timestamp_pb.Timestamp | undefined;
+  setRefreshTokenExpiresAt(value?: google_protobuf_timestamp_pb.Timestamp): LoginResponse;
+  hasRefreshTokenExpiresAt(): boolean;
+  clearRefreshTokenExpiresAt(): LoginResponse;
+
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): LoginResponse.AsObject;
   static toObject(includeInstance: boolean, msg: LoginResponse): LoginResponse.AsObject;
@@ -42,6 +62,11 @@ export class LoginResponse extends jspb.Message {
 export namespace LoginResponse {
   export type AsObject = {
     user?: user_pb.User.AsObject,
+    sessionId: string,
+    accessToken: string,
+    refreshToken: string,
+    accessTokenExpiresAt?: google_protobuf_timestamp_pb.Timestamp.AsObject,
+    refreshTokenExpiresAt?: google_protobuf_timestamp_pb.Timestamp.AsObject,
   }
 }
 
