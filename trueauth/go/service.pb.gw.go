f04e9a6cf859a20174943509536eb65b0128d5db
Ankur Kumar <kankurcool@gmail.com>
2023-04-05 15:26:23 +0530

    updated

--- trueauth/go/service.pb.gw.go
+++ trueauth/go/service.pb.gw.go
@@ -135,18 +135,15 @@ func local_request_TrueAuth_Login_0(ctx context.Context, marshaler runtime.Marsh
 
 }
 
-var (
-	filter_TrueAuth_VerifyEmail_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
 func request_TrueAuth_VerifyEmail_0(ctx context.Context, marshaler runtime.Marshaler, client TrueAuthClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq VerifyEmailRequest
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_TrueAuth_VerifyEmail_0); err != nil {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -159,10 +156,11 @@ func local_request_TrueAuth_VerifyEmail_0(ctx context.Context, marshaler runtime
 	var protoReq VerifyEmailRequest
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_TrueAuth_VerifyEmail_0); err != nil {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
