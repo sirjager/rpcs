22eca3327b1eeebc0d08d1bc84733ac05bdd1f8d
Ankur Kumar <kankurcool@gmail.com>
2023-04-05 15:27:31 +0530

    updated

--- emails/js/service.emails_pb.d.ts
+++
@@ -1,8 +0,0 @@
-import * as jspb from 'google-protobuf'
-
-import * as google_api_annotations_pb from './google/api/annotations_pb';
-import * as protoc$gen$openapiv2_options_annotations_pb from './protoc-gen-openapiv2/options/annotations_pb';
-import * as rpc$health_emails_pb from './rpc-health.emails_pb';
-import * as rpc$send_emails_pb from './rpc-send.emails_pb';
-
-
